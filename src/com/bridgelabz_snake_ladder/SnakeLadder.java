3fd7ffecf55031e80495dc8f157e879282d433cc
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-08 14:35:04 +0530

    Added : Java Programme For Snake and Ladder game

---
+++ src/com/bridgelabz_snake_ladder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz_snake_ladder;
+
+public class SnakeLadder {
+    public static void main(String[] args)
+    {
+        System.out.println("Playing Snake and Ladder");
+    }
+    }
+
